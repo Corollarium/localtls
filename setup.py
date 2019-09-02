789018824b41eaf112762e2bc8ce9544a65631f5
Bruno <brunobg@gmail.com>
2019-09-02 18:17:50 -0300

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='localtls',
-      version='1.0',
+      version='1.0.1',
       description='DNS server for local TLS',
       author='Corollarium',
       author_email='email@corollarium.com',

78056de304187edd9c3d80590427eda5bb9b3d16
mrjones-plip <mrjones-plip@plip.com>
2023-04-16 22:38:26 -0700

    update readme to be ubuntu 22.04 current + more, add systemd files

---
+++ service.example.py
@@ -0,0 +1,1 @@
+python3 dnsserver.py --domain yourdomain.net --soa-master=ns1.yourdomain.net --soa-email=email@yourdomain.net --ns-servers=ns1.yourdomain.net,ns2.yourdomain.net --log-level ERROR --http-port 80 --http-index /somewhere/index.html

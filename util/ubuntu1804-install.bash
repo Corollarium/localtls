a5beab0267a536c5fa3ae1b91dc29b15c0aa484b
mrjones-plip <mrjones-plip@plip.com>
2023-04-24 16:21:03 -0700

    restore old install script, rename new and old scripts to their respective ubuntu versoins

---
+++ util/ubuntu1804-install.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "This script installs deps for running depserver on a Ubuntu server."
+
+# base/certbot ppa
+echo "Getting updates"
+sudo apt-get install software-properties-common
+sudo add-apt-repository universe
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+
+# packages
+echo "Installing updates"
+sudo apt install mosh python3-pip certbot
+sudo pip3 install dnslib cherrypy
+
+# kill resolved
+echo "Removing resolved"
+echo "127.0.0.1 $(hostname)" >> /etc/hosts
+sudo systemctl disable systemd-resolved
+sudo systemctl stop systemd-resolved
+rm -f /etc/resolv.conf
+echo "nameserver 127.0.0.1" > /etc/resolv.conf
+
+sudo ufw allow ssh
+sudo ufw allow 53
+sudo ufw allow 80
+sudo ufw allow 443
+sudo ufw allow 60000:61000/udp
+sudo ufw enable
+
+echo "Ready. Now run python3 dnsserver.py"
